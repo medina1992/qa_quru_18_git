dcf0f4da4a305b25da8d4f2920c5923d95fb6842
Medina_Akhundova <medina.mstudio@gmail.com>
2025-04-12 16:48:30 +0300

    Hello QA-QURU, еще разок

--- src/main/java/com/medina/Main.java
+++ src/main/java/com/medina/Main.java
@@ -3,7 +3,7 @@ package com.medina;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello QA-QURU");
+        System.out.printf("Hello QA-QURU, еще разок");
 
 
         for (int i = 1; i <= 5; i++) {
