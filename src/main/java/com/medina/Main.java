8d5b7696167fb9f817eb443cf13ef967f9eacd1e
medina1992 <115163767+medina1992@users.noreply.github.com>
2025-04-12 17:35:14 +0300

    Update Main.java

--- src/main/java/com/medina/Main.java
+++ src/main/java/com/medina/Main.java
@@ -1,9 +1,11 @@
 package com.medina;
 
 public class Main {
+
+     public static String msg = "Hello QA-QURU!!!!!!!";
     public static void main(String[] args) {
 
-        System.out.printf("Hello QA-QURU!!!!!!!");
+        System.out.printf(msg);
 
 
         for (int i = 1; i <= 5; i++) {
